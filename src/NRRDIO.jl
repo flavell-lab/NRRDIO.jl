502d11df0d9c7f81f6700cc7623d94eb6639c430
Jungsoo Kim <urlicht@users.noreply.github.com>
2021-12-31 12:25:29 -0500

    Update NRRDIO.jl
    modify type dict

--- src/NRRDIO.jl
+++ src/NRRDIO.jl
@@ -1,7 +1,23 @@
 module NRRDIO
 using CodecZlib, TranscodingStreams, DataStructures
 
-const DICT_DTYPE_W=Dict{DataType,String}(UInt8=>"uint8",
+const DICT_DTYPE_W = Dict{String,DataType}("uint8"=>UInt8,
+    "int8"=>Int8,
+    "uint16"=>UInt16,
+    "int16"=>Int16,
+    "uin32"=>UInt32,
+    "int32"=>Int32,
+    "uint64"=>UInt64,
+    "int64"=>Int64,
+    "float"=>Float32,
+    "double"=>Float64,
+    
+    "unsigned char"=>UInt8,
+    "char"=>Int8,
+    "unsigned short"=>UInt16,
+    "short"=>Int16)
+
+const DICT_DTYPE_R = Dict{DataType,String}(UInt8=>"uint8",
     Int8=>"int8",
     UInt16=>"uint16",
     Int16=>"int16",
@@ -10,9 +26,7 @@ const DICT_DTYPE_W=Dict{DataType,String}(UInt8=>"uint8",
     UInt64=>"uint64",
     Int64=>"int64",
     Float32=>"float",
-    Float64=>"double")
-const DICT_DTYPE_R=Dict{String,DataType}([v => k for (k,v) = DICT_DTYPE_W]);
-
+    Float64=>"double");
 include("nrrd.jl")
 
 export nrrd_header,
